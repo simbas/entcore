5b76b8253d74a6acb162b92890989187861d43f8
Rafik Djedjig <rafik.djedjig@web-education.net>
2017-09-08 18:38:11 +0200

    [Feeder] [fix] Change soft errors report to ease context error highlight

--- feeder/src/main/java/org/entcore/feeder/utils/Validator.java
+++ feeder/src/main/java/org/entcore/feeder/utils/Validator.java
@@ -239,7 +239,7 @@ public class Validator {
 				if (errorsContext != null) {
 					errorsContext.addObject(new JsonObject()
 							.putString("reason", "missing.attribute")
-							.putString("attribute", i18n.translate(o.toString(), I18n.DEFAULT_DOMAIN, acceptLanguage))
+							.putString("attribute", o.toString())
 					);
 				}
 				return i18n.translate("missing.attribute", I18n.DEFAULT_DOMAIN, acceptLanguage,
@@ -424,7 +424,7 @@ public class Validator {
 			if (errorsContext != null) {
 				errorsContext.addObject(new JsonObject()
 						.putString("reason", "invalid.value")
-						.putString("attribute", i18n.translate(attr, I18n.DEFAULT_DOMAIN, acceptLanguage))
+						.putString("attribute", attr)
 						.putString("value", (value != null ? value.toString() : "null"))
 				);
 			}
