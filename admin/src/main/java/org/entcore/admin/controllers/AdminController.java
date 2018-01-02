d27a57826720f54ddeba8e620710f686a4dc8a69
david-cc <david.caudeli@web-education.net>
2018-01-02 15:48:55 +0100

    [admin-v2][CAV2 #187] Migrate user search methods to Directory

--- admin/src/main/java/org/entcore/admin/controllers/AdminController.java
+++ admin/src/main/java/org/entcore/admin/controllers/AdminController.java
@@ -1,13 +1,9 @@
 package org.entcore.admin.controllers;
 
-import org.entcore.admin.filters.AdminStructureFilter;
-import org.entcore.admin.services.AdminService;
-import org.entcore.admin.services.impl.AdminNeoService;
 import org.entcore.common.http.filter.AdminFilter;
 import org.entcore.common.http.filter.ResourceFilter;
 import org.vertx.java.core.http.HttpServerRequest;
 import org.vertx.java.core.json.JsonObject;
-import static org.entcore.common.http.response.DefaultResponseHandler.*;
 
 import fr.wseduc.rs.Get;
 import fr.wseduc.security.ActionType;
@@ -15,8 +11,6 @@ import fr.wseduc.security.SecuredAction;
 import fr.wseduc.webutils.http.BaseController;
 
 public class AdminController extends BaseController {
-
-	private final AdminService service = new AdminNeoService();
 
 	@Get("")
 	@SecuredAction(type = ActionType.RESOURCE, value = "")
@@ -30,28 +24,5 @@ public class AdminController extends BaseController {
 	@ResourceFilter(AdminFilter.class)
 	public void serveHomeAlias(HttpServerRequest request) {
 		serveHome(request);
-	}
-
-	@Get("api/structure/:id/quicksearch/users")
-	@SecuredAction(type = ActionType.RESOURCE, value = "")
-	@ResourceFilter(AdminStructureFilter.class)
-	public void quickSearchUsers(HttpServerRequest request) {
-		String structureId = request.params().get("id");
-		String input = request.params().get("input");
-
-		if(input == null || input.trim().length() == 0){
-			badRequest(request);
-			return;
-		}
-
-		this.service.quickSearchUsers(structureId, input, arrayResponseHandler(request));
-	}
-
-	@Get("api/structure/:id/users")
-	@SecuredAction(type = ActionType.RESOURCE, value = "")
-	@ResourceFilter(AdminStructureFilter.class)
-	public void userList(HttpServerRequest request) {
-		String structureId = request.params().get("id");
-		this.service.userList(structureId, arrayResponseHandler(request));
 	}
 }
